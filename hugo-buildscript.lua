ddb41725e4fc534a20b29622790c39ae78b39fb4
Hernan Dario Cano <dcanohdev@gmail.com>
2018-12-26 01:04:37 -0500

    Update hugo-buildscript.lua

--- hugo-buildscript.lua
+++ hugo-buildscript.lua
@@ -1,14 +1,30 @@
+require 'lide.base.init'
+
 print 'Checking if master and hugo-source exists:'
-os.execute "git checkout hugo-source &&"
-os.execute "git branch -D master &&"
-os.execute "git branch master &&"
-os.execute "git checkout master &&"
+os.execute "git checkout hugo-source"
+os.execute "git branch -D master"
+os.execute "git branch master"
+os.execute "git checkout master"
 
 print 'Executing Hugo:'
 os.execute "hugo"
 
-print 'Cleaning no html files:'
-os.execute "rm -rfv !(".git"|"public")"
+print 'Cleaning no html files:' --> no del public and .git
+
+for file in lide.base.lib.lfs.dir('.') do
+    if file ~= "." and file ~= ".." then
+        if (file ~= '.git' and file ~= 'public') then
+        	if lfs.attributes(file).mode == 'directory' then
+        		lide.folder.remove_tree(file)
+        	else
+        		lide.file.remove(file);
+        	end
+        end
+    end
+end
 
 print 'Copying files to rootdir:'
-os.execute "cp -a public/. ./"
+os.execute "xcopy public\\* .\\ /s /e" -- linux "cp -a public/. ./"
+
+-- git commit -a -m "Update public..."
+-- git push origin master
